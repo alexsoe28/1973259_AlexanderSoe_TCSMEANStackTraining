da093f14df1e337edc923c1138e2065a7f565426
Alex <alexndersoe28@gmail.com>
2021-03-29 16:04:28 -0700

    finished

--- AngularTestApp/AngularTestApp/src/app/quiz/quiz.component.ts
+++ AngularTestApp/AngularTestApp/src/app/quiz/quiz.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizQuestions } from 'src/quizQuestions';
 import { Question } from "src/question.model";
+
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
+  correctResponseMsg:string="Correct Answer";
+  incorrectResponseMsgs:Array<String>=[];
+  submitted:boolean=false;
+  selectedAnswers:Array<Number>=[];
   questionsArray:Array<Question>=[];
   correctAnswers:Array<Number>=[];
-  fractionCorrect:String = "";
+  fractionPass:String = "";
+  fractionFail:String = "";
   totalCorrect:Number = 0;
+  showQuizBreakdown:boolean = false;
   constructor(public service:QuizQuestions) { }
 
   ngOnInit(): void {
@@ -21,21 +28,40 @@ export class QuizComponent implements OnInit {
     for(let i = 0; i < this.questionsArray.length; i++){
       this.correctAnswers.push(this.questionsArray[i].CorrectAnswer);
     }
-    console.log(this.correctAnswers); 
   }
 
   callQuizQuestions(){
     this.service.loadQuizQuestions().subscribe(data=>this.questionsArray=data);
   }
+
   onQuizSubmit(userAnswers:any){
     this.findCorrectAnswers();
     let numCorrect = 0;
     for(let i = 1; i < this.questionsArray.length + 1; i++){
       let selectedAnswer = userAnswers.value[i];
+      this.selectedAnswers.push(selectedAnswer);
       if(selectedAnswer == this.correctAnswers[i - 1]){
         numCorrect += 1;
+        this.incorrectResponseMsgs.push(" ");
+      }
+      else{
+        if(selectedAnswer){
+          this.incorrectResponseMsgs.push("For Question " + i + " your selected answer: " + selectedAnswer + " is incorrect");
+        }
+        else{
+          this.incorrectResponseMsgs.push("For Question " + i + " you did not select an answer.");
+        }
       }
     }
-    this.fractionCorrect = numCorrect + "/10";
+    this.submitted = true;
+    if(numCorrect>7){
+      this.fractionPass = "Result: " + numCorrect + "/10: Pass!";
+    }
+    else{
+      this.fractionFail = "Result: " + numCorrect + "/10: Fail!";
+    }
+  }
+  quizBreakdown(){
+    this.showQuizBreakdown = true;
   }
 }
