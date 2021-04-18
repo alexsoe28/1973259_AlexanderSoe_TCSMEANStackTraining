10ba2939b1aca1b367a38c46d7cba6edddbea139
Alex <alexndersoe28@gmail.com>
2021-04-18 15:36:05 -0700

    finished

--- SocketChat/server.js
+++ SocketChat/server.js
@@ -1,7 +1,8 @@
-  
 let app = require("express")();
 let http = require("http").Server(app);   // to load the library we have run port number using hhtp module 
 let io = require("socket.io")(http);
+let mongoClient = require("mongodb").MongoClient;
+let url = "mongodb://localhost:27017"
 
 app.get("/",(req,res)=> {
     res.sendFile(__dirname+"/index.html");
@@ -9,9 +10,31 @@ app.get("/",(req,res)=> {
 
 io.on("connection",(socket)=> {
     console.log("Client connected to application.....");
-    
+    let name = "";
+    let message = "";
     socket.on("chat",(msg)=> {
-        console.log(msg);
+        if(msg.includes("Name:")){
+            name = msg.slice(6);
+        }
+        else{
+            message = msg.slice(9);
+        }
+        if(name && message){
+            mongoClient.connect(url, {useUnifiedTopology: true },(err1,client)=>{
+                if(!err1){
+                    let db = client.db("meanstack");
+                    db.collection("ChatLog").insertOne({name: name, message: message},(err2,result)=>{
+                            if(!err2){
+                                console.log(result.insertedCount + " message has been sent!");
+                            }else {
+                                console.log(err2.message);
+                            }
+                            client.close();    
+                        });
+                        
+                    }
+            });
+        }
     })
 })
 http.listen(9090,()=>console.log('server running on port number 9090'));
